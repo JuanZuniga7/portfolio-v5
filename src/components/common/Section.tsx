62edc4737a2f950d0e0c8f3d61ef85df41a967bd
Juan Zuñiga <rusted.cor79@gmail.com>
2024-05-10 17:02:52 -0500

    Refactor Section component to add key prop to SubSection elements

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -11,7 +11,7 @@ export default function Section({ section }: Props) {
         <div id="about" className="my-12 lg:my-16 relative">
             <SectionTitle name="About" reverse={section.reverse} />
             {section.subSections.map((subSection, index) => (
-                <SubSection profile={subSection.image} title={subSection.title} description={subSection.description} reverse={index % 2 === 1} />
+                <SubSection key={index} profile={subSection.image} title={subSection.title} description={subSection.description} reverse={index % 2 === 1} />
             ))}
         </div>
     )
